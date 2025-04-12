05990eae7cfbae4fbd0bf33ce16f204a183eb9cf
Benson Thomas <56196428+Benson96-git@users.noreply.github.com>
2025-04-13 01:43:43 +0530

    Update loancalculator.py

--- loancalculator.py
+++ loancalculator.py
@@ -1,58 +1,121 @@
-# import pickle
 import joblib
 import streamlit as st
 import pandas as pd
 import numpy as np
-# import warnings
-# warnings.filterwarnings("ignore")
+
 
 model_columns = joblib.load('./models/model_columns.pkl')
 model = joblib.load('./models/feature_model.pkl')
+def add_custom_title_style():
+    st.markdown(
+        """
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Lobster&display=swap');
+
+        .title-stylish{
+            font-family: 'Lobster', cursive; 
+            font-size: 40px; 
+            color: #2E86C1; 
+            text-align: center; 
+        }
+
+        .title-formal{
+            font-family: 'Arial', sans-serif;
+            font-size: 40px;
+            color: #2E86C1; 
+            text-align: center;
+            margin-bottom: 60px
+        }
+        </style><br><br>
+        """,
+        unsafe_allow_html=True
+    )
+def add_sidebar_footer_email():
+    st.sidebar.markdown(
+        """
+        <style>
+        .sidebar .sidebar-content {
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+        }
+        .sidebar-footer {
+            margin-top: auto; 
+            text-align: center; 
+            font-size: 14px; 
+            color: #555;
+        }
+        </style>
+        <div class="sidebar-footer">
+            <p>Need help? Contact us at:</p>
+            <a href="mailto:support@bankingapp.com">support@bankingapp.com</a>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 def run():
-    st.title('Predict Approval of your loan')
-   
+    add_custom_title_style()
+    st.markdown('<h1 class="title-formal">Predict Approval of your Loan</h1>', unsafe_allow_html=True)
+    # Sidebar
+    st.sidebar.markdown('<h2 class="title-stylish">Your Banking Partner!</h2>', unsafe_allow_html=True)
+    st.sidebar.markdown(
+        """
+        <style>
+        .sidebar .sidebar-content {
+            background-color: #f8f9fa;
+            padding: 20px;
+            border-radius: 10px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    st.sidebar.image('bank.png')
+
+    st.sidebar.write("* Use the form on the main page to predict loan approval.")
+    st.sidebar.write("* Ensure all fields are filled correctly.")
+    add_sidebar_footer_email()
+    # Main Page
     col1, col2, col3 = st.columns(3)
     #First row
     with col1:
-        no_of_dependents = st.number_input('Number of dependents', value=2)
+        no_of_dependents = st.number_input('Number of dependents',min_value=0, value=2)
     with col2:
         education = st.selectbox('Education', options=['Not Graduate', 'Graduate'])
     with col3:       
         self_employed = st.selectbox('Self Employed', options=['No', 'Yes'])
     #Second row
     with col1:
-        income_annum = st.number_input("Applicant's Annual Income", value=200000)
+        income_annum = st.number_input("Applicant's Annual Income",min_value=0, value=200000)
     with col2:
-        loan_amount = st.number_input("Loan Amount", value=10000)   
+        loan_amount = st.number_input("Loan Amount",min_value=0 ,value=10000)   
     with col3:
-        loan_term = st.number_input('Tenure(In Years)', value=0)
+        loan_term = st.number_input('Tenure(In Years)',min_value=0 ,value=1)
     #Third row
     with col1:
-        cibil_score =  st.number_input('Cibil score', value=410)
+        cibil_score =  st.number_input('Cibil score',min_value=0,max_value=900, value=410)
     with col2:
-        residential_assets_value = st.number_input('Residential Assets Value', value=15000)
+        residential_assets_value = st.number_input('Residential Assets Value',min_value=0, value=15000)
     with col3:
-        commercial_assets_value = st.number_input('Commercial Assests Value', value=16000)
+        commercial_assets_value = st.number_input('Commercial Assests Value',min_value=0, value=16000)
     #$th row
     with col1:
-        luxury_assets_value = st.number_input('Luxury Assets Value', value=15600)
+        luxury_assets_value = st.number_input('Luxury Assets Value',min_value=0, value=15600)
     with col2:
-        bank_asset_value = st.number_input('Bank Assets Value', value=18900)
+        bank_asset_value = st.number_input('Bank Assets Value',min_value=0, value=18900)
 
-    print('income_annum', income_annum)
-    print('no_of_dependents', no_of_dependents)
-    print('education', education)
-    print('self_employed', self_employed)
-    print('loan_amount', loan_amount)
-    print('loan_term', loan_term)
-    print('cibil_score', cibil_score)
-    print('residential_assets_value', residential_assets_value)
-    print('commercial_assets_value', commercial_assets_value)
-    print('luxury_assets_value', luxury_assets_value)
-    print('bank_asset_value', bank_asset_value)
-
-    if st.button('Submit'):
+    if st.button('Predict Loan Status'):
+        #validations
+        if loan_amount == 0:
+          st.error("Please fill out loan_amount field.")
+          return
+        if loan_amount > bank_asset_value:
+          st.error("Loan Amount cannot exceed Bank Asset Value.")
+          return
+        if loan_term == 0 and loan_amount > 0:
+          st.error("Loan Term must be greater than 0 if a Loan Amount is added.")
+          return
         user_input = {
     'no_of_dependents': no_of_dependents, 
     'education': education,
@@ -66,6 +129,7 @@ def run():
     'luxury_assets_value':luxury_assets_value, 
     'bank_asset_value':bank_asset_value,
         }
+        print(f"user_input: {user_input}")
         input_df = pd.DataFrame(user_input,index=range(0,1))
         input_encoded = pd.get_dummies(input_df)
 
